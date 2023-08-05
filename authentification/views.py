029b170575e213e977bfac2c0dc3c4448fd6883b
KNJSoft <knjprod@gmail.com>
2023-08-05 09:43:55 +0100

    modification de la zone d'affichage des messages d'erreurs ou succes

--- authentification/views.py
+++ authentification/views.py
@@ -26,31 +26,32 @@ def signup(request):
         password = request.POST['password']
         confirmpwd = request.POST['comfirmpwd']
         if User.objects.filter(username=username):
-            messages.error(request, 'username already taken please try another.')
-            return redirect('signup')
+            messages.add_message(request,messages.ERROR, 'username already taken please try another.')
+            return render(request,'authentification/signup.html',{'messages':messages.get_messages(request)})
+        #messages.error(request,'messages error') affiche le msg dans l'interface admin,il faut configurer dans l'interface utilisateurs
         if User.objects.filter(email=email):
-            messages.error(request, 'This email has an account.')
-            return redirect('signup')
+            messages.add_message(request,messages.ERROR, 'This email has an account.')
+            return render(request,'authentification/signup.html',{'messages':messages.get_messages(request)})
         if len(username)>10:
-            messages.error(request, 'Please the username must not be more than 10 character.')
-            return redirect('signup')
+            messages.add_message(request,messages.ERROR, 'Please the username must not be more than 10 character.')
+            return render(request,'authentification/signup.html',{'messages':messages.get_messages(request)})
         if len(username)<5:
-            messages.error(request, 'Please the username must be at leat 5 characters.')
-            return redirect('signup')
+            messages.add_message(request,messages.ERROR, 'Please the username must be at leat 5 characters.')
+            return render(request,'authentification/signup.html',{'messages':messages.get_messages(request)})
         if not username.isalnum():
-            messages.error(request, 'username must be alphanumeric')
-            return redirect('signup')
+            messages.add_message(request,messages.ERROR, 'username must be alphanumeric')
+            return render(request,'authentification/signup.html',{'messages':messages.get_messages(request)})
 
         if password != confirmpwd:
-            messages.error(request, 'The password did not match! ')  
-            return redirect('signup')                  
+            messages.add_message(request,messages.ERROR, 'The password did not match! ')
+            return render(request,'authentification/signup.html',{'messages':messages.get_messages(request)})
 
         my_user = User.objects.create_user(username, email, password)
         my_user.first_name =firstname
         my_user.last_name = lastname
         my_user.is_active = False
         my_user.save()
-        messages.success(request, 'Your account has been successfully created. we have sent you an email You must comfirm in order to activate your account.')
+        messages.add_message(request,messages.SUCCESS, 'Your account has been successfully created. we have sent you an email You must comfirm in order to activate your account.')
 # send email when account has been created successfully
         subject = "Welcome to django-application donaldPro"
         message = "Welcome "+ my_user.first_name + " " + my_user.last_name + "\n thank for chosing Dprogrammeur website for test login.\n To order login you need to comfirm your email account.\n thanks\n\n\n donald programmeur"
@@ -78,7 +79,7 @@ def signup(request):
 
         email.fail_silently = False
         email.send()
-        return redirect('signin')
+        return render(request,'authentification/signin.html',{'messages':messages.get_messages(request)})
     return render(request, 'authentification/signup.html')    
 
 
@@ -94,11 +95,11 @@ def signin(request):
             firstname = user.first_name
             return render(request, 'authentification/index.html', {"firstname":firstname})
         elif my_user.is_active == False:
-            messages.error(request, 'you have not confirm your  email do it, in order to activate your account')  
-            return redirect('signin')  
+            messages.add_message(request,messages.ERROR, 'you have not confirm your  email do it, in order to activate your account')
+            return render(request,'authentification/signin.html')
         else:
-            messages.error(request, 'bad authentification')
-            return redirect('home') 
+            messages.add_message(request,messages.ERROR, 'bad authentification')
+            return render(request,'authentification/index.html',{'messages':messages.get_messages(request)})
 
     return render(request, 'authentification/signin.html')    
 
@@ -117,8 +118,8 @@ def activate(request, uidb64, token):
     if my_user is not None and generateToken.check_token(my_user, token):
         my_user.is_active  = True        
         my_user.save()
-        messages.success(request, "You are account is activated you can login by filling the form below.")
-        return redirect("signin")
+        messages.add_message(request,messages.SUCCESS, "You are account is activated you can login by filling the form below.")
+        return render(request,"authentification/signin.html",{'messages':messages.get_messages(request)})
     else:
-        messages.success(request, 'Activation failed please try again')
-        return redirect('home')
+        messages.add_message(request,messages.ERROR, 'Activation failed please try again')
+        return render(request,'authentification/index.html',{'messages':messages.get_messages(request)})
